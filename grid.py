ef6b44e9bd29ec459273a59711a0b2707822a8d8
gsactown30 <gsactown30@gmail.com>
2026-04-09 14:57:42 -0700

    changed to allow both 2d and 3d rendering of scans

--- grid.py
+++ grid.py
@@ -2,13 +2,13 @@ import numpy as np
 import open3d as o3d
 
 def convertOrigin(pose):
-    originX = (pose[0, 3] / 0.2).astype(int) + 1000
-    originY = (pose[1, 3] / 0.2).astype(int) + 1000
+    originX = (pose[0, 3] / 0.2).astype(int) + 200
+    originY = (pose[1, 3] / 0.2).astype(int) + 200
     #originZ = (pose[2, 3] / 0.2).astype(int) + 10
     origin = (originX, originY)
     return origin
 
-def convertCoordAll(scan, pose):
+def convertCoordAll3D(scan, pose):
     booleanMask = (np.abs(scan[:, 0]) < 30) & (np.abs(scan[:, 1]) < 30)
     newScan = scan[booleanMask]
     newScan[:, 3] = 1
@@ -26,16 +26,37 @@ def convertCoordAll(scan, pose):
     removalMask = np.abs((newScan[:, 0] * equation[0]) + (newScan[:, 1] * equation[1]) + (newScan[:, 2] * equation[2]) + (equation[3])) > 0.1
     newScan = newScan[removalMask]
 
-    coordX = (newScan[:, 0] / 0.2).astype(int) + 1000
-    coordY = (newScan[:, 1] / 0.2).astype(int) + 1000
+    coordX = (newScan[:, 0] / 0.2).astype(int) + 200
+    coordY = (newScan[:, 1] / 0.2).astype(int) + 200
     coordZ = (newScan[:, 2] / 0.2).astype(int) + 10
     return np.column_stack((coordX, coordY, coordZ))
 
+def convertCoordAll2D(scan, pose):
+    booleanMask = (np.abs(scan[:, 0]) < 30) & (np.abs(scan[:, 1]) < 30)
+    newScan = scan[booleanMask]
+    newScan[:, 3] = 1
+    newScan = np.dot(newScan, pose.T)
+    heightMask = (newScan[:, 2] < 2) & (newScan[:, 2] > -2)
+    newScan = newScan[heightMask]
+
+    groundMask = (newScan[:, 2] < -1.5) & (newScan[:, 2] > -3.5)
+    groundPoints = newScan[groundMask]
+    groundPoints = groundPoints[:, :3]
+    pcdGround = o3d.geometry.PointCloud()
+    pcdGround.points = o3d.utility.Vector3dVector(groundPoints)
+    groundPlane = pcdGround.segment_plane(0.01, 3, 10)
+    equation, pointsInPlane = groundPlane
+    removalMask = np.abs((newScan[:, 0] * equation[0]) + (newScan[:, 1] * equation[1]) + (newScan[:, 2] * equation[2]) + (equation[3])) > 0.1
+    newScan = newScan[removalMask]
+
+    coordX = (newScan[:, 0] / 0.2).astype(int) + 200
+    coordY = (newScan[:, 1] / 0.2).astype(int) + 200
+    return np.column_stack((coordX, coordY))
+
 class Grid():
-    def __init__(self, xAxis, yAxis, zAxis):
+    def __init__(self, xAxis, yAxis, zAxis = 1):
         self.array = np.zeros((zAxis, xAxis, yAxis))
         self.xAxis = xAxis
         self.yAxis = yAxis
         self.zAxis = zAxis
-        self.zAxis = 0
         #self.center = (xAxis//2, yAxis//2)
--- sensor_model.py
+++ sensor_model.py
@@ -1,72 +1,103 @@
 import pykitti
 import numpy as np
 from grid import Grid
-from grid import convertCoordAll
+from grid import convertCoordAll2D
+from grid import convertCoordAll3D
 from grid import convertOrigin
 from raycast import bresenham
-import matplotlib.pyplot as plt
 import time
 import open3d as o3d
+import matplotlib.pyplot as plt
 
 startTime = time.perf_counter()
 
 basedir = './data/kitti'
 date = '2011_09_26'
 drive = '0015'
-
 dataset = pykitti.raw(basedir, date, drive)
-occupancyGrid = Grid(4000, 4000, 50)
-scan = dataset.get_velo(165)
-print(f"Max x: {scan[:,0].max():.1f}  Min x: {scan[:,0].min():.1f}")
-print(f"Max y: {scan[:,1].max():.1f}  Min y: {scan[:,1].min():.1f}")
-print(f"Max z: {scan[:,2].max():.1f}  Min z: {scan[:,2].min():.1f}")
 
-for i in range(0, 1):
-    scan = dataset.get_velo(i)
-    pose = dataset.oxts[i].T_w_imu
-    newScan = convertCoordAll(scan, pose)
-    origin = convertOrigin(pose)
+choice = int(input('2d(1) or 3d(2) scan'))
+startScan = int(input('starting scan (0 for default)'))
+maxScan = startScan + int(input('how many scans'))
 
-    arrFree = []
-    arrHit = []
+if choice == 1:
+    occupancyGrid = Grid(2000, 2000)
+    print(startScan)
+    print(maxScan)
 
-    for point in newScan:
-        coord = (point[0], point[1])
-        coordHit = (point[0], point[1], point[2])
-        arrFree.extend(bresenham(origin, coord))
-        arrHit.append(coordHit)
+    for i in range(startScan, maxScan):
+        scan = dataset.get_velo(i)
+        pose = dataset.oxts[i].T_w_imu
+        newScan = convertCoordAll2D(scan, pose)
+        origin = convertOrigin(pose)
 
-    arrFree = np.array(arrFree)
-    np.add.at(occupancyGrid.array, (slice(None), arrFree[:, 0], arrFree[:, 1]), -0.2)
+        arrFree = []
+        arrHit = []
 
-    arrHit = np.array(arrHit)
-    np.add.at(occupancyGrid.array, (arrHit[:, 2], arrHit[:, 0], arrHit[:, 1]), 1)
-    occupancyGrid.array = np.clip(occupancyGrid.array, -10, 10)
+        for point in newScan:
+            coord = (point[0], point[1])
+            arrFree.extend(bresenham(origin, coord))
+            arrHit.append(coord)
 
-probabilities = 1 / (1 + np.exp(-(occupancyGrid.array)))
-occupied = np.where(probabilities > 0.6)
-ocZ, ocX, ocY = occupied
+        arrFree = np.array(arrFree)
+        np.add.at(occupancyGrid.array, (0, arrFree[:, 0], arrFree[:, 1]), -0.2)
 
-ocZ = (ocZ - 25) * 0.2
-ocX = (ocX - 1000) * 0.2
-ocY = (ocY - 1000) * 0.2
-newOccupied = np.column_stack((ocX, ocY, ocZ))
+        arrHit = np.array(arrHit)
+        np.add.at(occupancyGrid.array, (0, arrHit[:, 0], arrHit[:, 1]), 2)
+        occupancyGrid.array = np.clip(occupancyGrid.array, -10, 10)
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(newOccupied)
-#o3d.geometry.PointCloud.segment_plane()
-o3d.visualization.draw_geometries([pcd])
+    probabilities = 1 / (1 + np.exp(-(occupancyGrid.array)))
+    probabilities = np.sum(probabilities, 0)
 
-print(occupancyGrid.array.max())
-print(occupancyGrid.array.min())
-print(np.count_nonzero(occupancyGrid.array))
-'''
-plt.imshow(probabilities, cmap='binary', interpolation='nearest', vmax=1, vmin=0, origin="lower")
-plt.colorbar()
-plt.savefig("testGrid")
-'''
+    plt.imshow(probabilities, cmap='binary', interpolation='nearest', vmax=1, vmin=0, origin="lower")
+    plt.colorbar()
+    plt.savefig("testGrid")
 
-endTime = time.perf_counter()
-print(f"Program runtime: {endTime - startTime:.2f} seconds")
+    endTime = time.perf_counter()
+    print(f"Program runtime: {endTime - startTime:.2f} seconds")
 
-#plt.show()
+    plt.show()
+
+elif choice == 2:
+    occupancyGrid = Grid(2000, 2000, 20)
+
+    for i in range(startScan, maxScan):
+        scan = dataset.get_velo(i)
+        pose = dataset.oxts[i].T_w_imu
+        newScan = convertCoordAll3D(scan, pose)
+        origin = convertOrigin(pose)
+
+        arrFree = []
+        arrHit = []
+
+        for point in newScan:
+            coord = (point[0], point[1])
+            coordHit = (point[0], point[1], point[2])
+            arrFree.extend(bresenham(origin, coord))
+            arrHit.append(coordHit)
+
+        arrFree = np.array(arrFree)
+        np.add.at(occupancyGrid.array, (slice(None), arrFree[:, 0], arrFree[:, 1]), -0.2)
+
+        arrHit = np.array(arrHit)
+        np.add.at(occupancyGrid.array, (arrHit[:, 2], arrHit[:, 0], arrHit[:, 1]), 2)
+        occupancyGrid.array = np.clip(occupancyGrid.array, -10, 10)
+
+    probabilities = 1 / (1 + np.exp(-(occupancyGrid.array)))
+    occupied = np.where(probabilities > 0.6)
+    ocZ, ocX, ocY = occupied
+
+    ocZ = (ocZ - 10) * 0.2
+    ocX = (ocX - 200) * 0.2
+    ocY = (ocY - 200) * 0.2
+    newOccupied = np.column_stack((ocX, ocY, ocZ))
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(newOccupied)
+    #o3d.geometry.PointCloud.segment_plane()
+    pcd.remove_statistical_outlier(20, 2, True)
+
+    endTime = time.perf_counter()
+    print(f"Program runtime: {endTime - startTime:.2f} seconds")
+
+    o3d.visualization.draw_geometries([pcd])
